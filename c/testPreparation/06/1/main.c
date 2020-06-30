58ac3bb6fdf9faea0e2dac613723198f257cbf7e
Gligor Grigorov <grigorovgligor@gmail.com>
2020-06-30 20:07:07 +0200

    Add simple cp program

---
+++ c/testPreparation/06/1/main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <err.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+	if(argc != 3)
+		errx(1,"Wrong number of arguments");		
+	int fd1;
+	if((fd1 = open(argv[1], O_RDONLY)) == -1)
+		err(2,"Error opening file %s", argv[1]);	
+	
+	int fd2;
+	if((fd2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR )) == -1)
+		err(3,"Error opening file %s", argv[2]);
+	char buf;
+	int size;
+	while((size = read(fd1, &buf, sizeof(buf))) > 0)
+	{
+		if(write(fd2, &buf, size) == -1)
+			err(4,"Error writing to file %s", argv[2]);
+	}
+	close(fd1);
+	close(fd2);
+	exit(0);
+}
